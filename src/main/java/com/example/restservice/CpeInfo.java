8a643d4ba416a96c760fb0c066fa28e5687465c1
Marcin Gizka <mgizka@mgizka-ltms36e.internal.salesforce.com>
2021-07-23 07:46:25 +0200

    mm

---
+++ src/main/java/com/example/restservice/CpeInfo.java
@@ -0,0 +1,21 @@
+
+package com.example.restservice;
+
+public class CpeInfo {
+
+	private final String msisdn;
+	private final String content;
+
+	public Invoice(String msisdn) {
+		this.id = id;
+		this.content = "{ /"msisdn/" : " + msiddn + "," + "/"cpemodel/" : /"samsung/" , "/"cpeversion/" : /"1.2.2/" : }";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
