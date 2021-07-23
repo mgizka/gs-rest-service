4acd1fd935070ad7f57272f403dfd1255cf03bcc
Marcin Gizka <mgizka@mgizka-ltms36e.internal.salesforce.com>
2021-07-23 08:30:05 +0200

    mm

--- src/main/java/com/example/restservice/CpeInfoController.java
+++ src/main/java/com/example/restservice/CpeInfoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CpeInfoontroller {
+public class CpeInfoController {
 
 	@GetMapping("/cpeinfo")
 	public CpeInfo cpeinfo(@RequestParam(value = "msisdn") String msisdn) {
