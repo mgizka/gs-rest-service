f7950a373f0bb2a536229851da33903cfa8ab0a6
Marcin Gizka <mgizka@mgizka-ltms36e.internal.salesforce.com>
2021-07-23 08:33:19 +0200

    mm

--- src/main/java/com/example/restservice/CpeInfo.java
+++ src/main/java/com/example/restservice/CpeInfo.java
@@ -4,15 +4,26 @@ package com.example.restservice;
 public class CpeInfo {
 
 	private final String msisdn;
-
+	private final String cpeversion;
+	private final String cpemodel;
 
 
 	public CpeInfo(String msisdn) {
 		this.msisdn = msisdn;
+		cpeversion = "1.2.2";
+		cpemodel = "Samsung Galaxy 20 Ultra Note";
 	}
 
 	public String getMsisdn() {
 		return msisdn;
+	}
+
+	public String getCpeModel() {
+		return cpemodel;
+	}
+
+	public String getCpeVersion() {
+		return cpeversion;
 	}
 
 }
