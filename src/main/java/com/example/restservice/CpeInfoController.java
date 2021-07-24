42834a34b725031a39f8e4837ecead4182abd96b
Marcin Gizka <mgizka@mgizka-ltms36e.internal.salesforce.com>
2021-07-24 18:24:07 +0200

    aa

--- src/main/java/com/example/restservice/CpeInfoController.java
+++ src/main/java/com/example/restservice/CpeInfoController.java
@@ -1,7 +1,7 @@
 package com.example.restservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
