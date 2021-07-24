245df8109bf37401d7c2d14d200ef8982b5c8408
Marcin Gizka <mgizka@mgizka-ltms36e.internal.salesforce.com>
2021-07-24 18:53:24 +0200

    aa

---
+++ src/main/java/com/example/restservice/DbUtil.java
@@ -0,0 +1,9 @@
+
+
+public class DbUtil {
+
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        return DriverManager.getConnection(dbUrl);
+    }
+}
